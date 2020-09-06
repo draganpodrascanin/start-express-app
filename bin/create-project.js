e28f3f36108caa1625db5c8eef94b6b3b008858c
macbook <macbook@xs-MacBook-Pro.local>
2020-09-07 00:17:50 +0200

    template refactoring

--- bin/create-project.js
+++ bin/create-project.js
@@ -3,6 +3,6 @@ require = require('esm')(module /*, options*/);
 
 const argv = require('yargs').argv;
 
-// console.log(argv)
+// console.log(argv).
 console.log(process.argv);
 require('../src/cli').cli(process.argv, argv);
